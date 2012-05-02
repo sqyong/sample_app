c5b28464c7afdd51eb79546970d1f6b2b4e51c20
S Q YONG <emailshaoqin@gmail.com>
2012-05-02 22:08:59 +0800

    user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@
 #  id         :integer         not null, primary key
 #  name       :string(255)
 #  email      :string(255)
+
 #  created_at :datetime        not null
 #  updated_at :datetime        not null
 #
